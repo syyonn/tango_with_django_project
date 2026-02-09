a5d24005bfff2c9bd415a382eb92df24bcaa5d0c
Syon Chotai <2970150c@student.gla.ac.uk>
2026-02-09 02:13:08 +0000

    added cat.jpg and new about.html template

--- rango/views.py
+++ rango/views.py
@@ -11,8 +11,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse(
-        "Rango says here is the about page."
-        "<br>"
-        "<a href='/rango/'>Index</a>"
-        )
+    context_dict = {'boldmessage': 'This tutorial has been put together by Syon.'}
+    return render(request, 'rango/about.html', context=context_dict)
