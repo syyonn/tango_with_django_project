b9a66210350c6678c6d374c15239a17c857e1d88
Syon Chotai <2970150c@student.gla.ac.uk>
2026-02-11 23:39:12 +0000

    imported the same thing twice in admin.py

--- rango/admin.py
+++ rango/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
 # Register your models here.
-
-from django.contrib import admin
-from rango.models import Category, Page
 
 admin.site.register(Category)
 admin.site.register(Page)
