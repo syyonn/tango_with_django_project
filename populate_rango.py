03602df47d02726f6b4c784b9e764e213002d2cd
Syon Chotai <2970150c@student.gla.ac.uk>
2026-02-12 04:41:02 +0000

    populate_rango.py actually works as intended now!!!

--- populate_rango.py
+++ populate_rango.py
@@ -35,9 +35,15 @@ def populate():
         {'title':'Flask',
          'url':'http://flask.pocoo.org'} ]
 
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages,
+                       'views': 128,
+                       'likes': 64},
+            'Django': {'pages': django_pages,
+                       'views': 64,
+                       'likes': 32},
+            'Other Frameworks': {'pages': other_pages,
+                                 'views': 32,
+                                 'likes': 16} }
     
     # If you want to add more categories or pages,
     # add them to the dictionaries above.
@@ -46,7 +52,7 @@ def populate():
     # and then adds all the associated pages for that category.
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -62,8 +68,8 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     c.save()
     return c
 
--- rango/admin.py
+++ rango/admin.py
@@ -3,5 +3,20 @@ from rango.models import Category, Page
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Page)
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,              {'fields': ['name']}),
+        ('Social Features', {'fields': ['views', 'likes']})
+    ]
+    list_display = ('name', 'views', 'likes')
+
+class PageAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Information', {'fields': ['category', 'url']}),
+        ('Social Features', {'fields': ['views']})
+    ]
+    list_display = ('title', 'category', 'url', 'views')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin) # category, title, url, views
