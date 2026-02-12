7ada9d90baff5f0c7741ae5b4b43580212709db2
Syon Chotai <2970150c@student.gla.ac.uk>
2026-02-12 00:46:44 +0000

    populate_rango script added

--- rango/models.py
+++ rango/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
     
     def __str__(self):
         return self.name
@@ -13,6 +16,6 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.title
