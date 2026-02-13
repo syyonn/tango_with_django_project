fe95d10bb784f039ddee5c0a6707a3f054e71114
Syon Chotai <2970150c@student.gla.ac.uk>
2026-02-13 14:17:15 +0000

    added sidebar

--- rango/views.py
+++ rango/views.py
@@ -21,6 +21,8 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
+    print(request.method)
+    print(request.user)
     context_dict = {'boldmessage': 'This tutorial has been put together by Syon.'}
     return render(request, 'rango/about.html', context=context_dict)
 
